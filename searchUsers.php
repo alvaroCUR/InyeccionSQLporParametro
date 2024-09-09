fda9beefa2b2aebd84e3c67d12b9fbef96e31210
alvaroCUR <47195715+alvaroCUR@users.noreply.github.com>
2024-09-09 23:45:43 +0200

    Update searchUsers.php

--- searchUsers.php
+++ searchUsers.php
@@ -1,9 +1,9 @@
 <?php
 
   $server = "localhost";
-  $username= "lyoz"; //nombre de usuario que se conecta a la base de datos
-  $password= "lykoz123"; //contraseña del usuario de la base de datos
-  $database = "hack4u"; //nombre de la base de datos
+  $username= "usuario"; //nombre de usuario que se conecta a la base de datos
+  $password= "contraseña"; //contraseña del usuario de la base de datos
+  $database = "basededatos"; //nombre de la base de datos
 
   //conexion a la base de datos
   $conn = new mysqli($server, $username, $password, $database);
